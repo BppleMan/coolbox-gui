30c8f691a25c10c0a6d839d91a2f0d4696ee8cf9
丘开宇 <qky1412@boostfield.com>
2023-11-08 21:01:20 +0800

    WIP expansion style

--- src/app/cool-card/cool-card.component.ts
+++ src/app/cool-card/cool-card.component.ts
@@ -6,31 +6,48 @@ import {MatRippleModule} from "@angular/material/core"
 import {MatExpansionModule, MatExpansionPanel} from "@angular/material/expansion"
 import {MatIconModule} from "@angular/material/icon"
 import {MatStepperModule} from "@angular/material/stepper"
+import {MatDividerModule} from "@angular/material/divider"
 import {Cool} from "../model/models"
+import {HIGHLIGHT_OPTIONS, HighlightModule} from "ngx-highlightjs"
 
 @Component({
     selector: "app-cool-card",
     standalone: true,
-    imports: [CommonModule, MatButtonModule, MatCheckboxModule, MatExpansionModule, MatIconModule, MatStepperModule, MatRippleModule],
+    imports: [CommonModule, MatButtonModule, MatCheckboxModule, MatExpansionModule, MatIconModule, MatStepperModule, MatRippleModule, HighlightModule, MatDividerModule],
     templateUrl: "./cool-card.component.html",
     styleUrls: ["./cool-card.component.scss"],
+    providers: [
+        {
+            provide: HIGHLIGHT_OPTIONS,
+            useValue: {
+                coreLibraryLoader: () => import("highlight.js/lib/core"),
+                languages: {
+                    typescript: () => import("highlight.js/lib/languages/typescript"),
+                    bash: () => import("highlight.js/lib/languages/bash"),
+                },
+            },
+        },
+    ]
 })
 export class CoolCardComponent {
     @ViewChild("expansionPanel") panel!: MatExpansionPanel
     @Input() cool!: Cool
     selected = false
-
-
+    expanded = false
+    consoleCode = 'mkdir xxx-project && cp a b'
     constructor() {
     }
 
-    toggle_panel(event: MouseEvent) {
-        event.stopPropagation()
-        this.panel.toggle()
-    }
+    // toggle_panel(event: MouseEvent) {
+    //     event.preventDefault()
+    //     event.stopPropagation()
+    //     this.panel.toggle()
+    // }
 
-    toggle_selected(event: MouseEvent) {
-        event.stopPropagation()
-        this.selected = !this.selected
+    toggle_panel(event: MouseEvent) {
+        console.log('toggle_selected')
+        event.preventDefault()
+        this.expanded = !this.expanded
+        this.panel.toggle()
     }
 }
